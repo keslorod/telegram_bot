c3ad4b1cf08760edf0998098cef5d525f2ae9655
DozZzeR <alex.dozer@gmail.com>
2017-10-17 10:34:18 +0300

    17.10 DB Handler добавлен

--- src/main/java/click/dozer/DBHandler.java
+++ src/main/java/click/dozer/DBHandler.java
@@ -1,7 +1,25 @@
 package click.dozer;
 
+import org.sqlite.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by alexd on 16.10.2017.
  */
 public class DBHandler {
+    private static DBHandler instance = null;
+    private Connection connection;
+
+    private DBHandler() throws SQLException {
+        DriverManager.registerDriver(new JDBC());
+        this.connection = DriverManager.getConnection(Config.DB_URL);
+    }
+
+    public static synchronized DBHandler getInstance() throws SQLException {
+        if (instance == null) instance = new DBHandler();
+        return instance;
+    }
 }
